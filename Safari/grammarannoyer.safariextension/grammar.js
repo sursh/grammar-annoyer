bd33f7ed87f9b4d60fdf6b0273969d2d306a3e9e
Andrew Abdalian <abdalian@Splinter.local>
2013-04-01 13:07:55 -0400

    Updated safari source and safariextz
    Incorporated pull request for backtracing for capitalization

--- Safari/grammarannoyer.safariextension/grammar.js
+++ Safari/grammarannoyer.safariextension/grammar.js
@@ -25,27 +25,19 @@ function walk(node) {
 }
 
 function handleText(textNode)  {
-var randnum = Math.floor((Math.random()*10)+1);
+	var randnum = Math.floor((Math.random()*10)+1);
 	if (randnum == 10){
 
 		var v = textNode.nodeValue;
 
-		v = v.replace(/\bYour\b/g, "You're");
-		v = v.replace(/\byour\b/g, "you're");
-		v = v.replace(/\bDefinitely\b/g, "Definately");
-		v = v.replace(/\bdefinitely\b/g, "definately");
-		v = v.replace(/\bTheir\b/g, "There");
-		v = v.replace(/\btheir\b/g, "there");
-		v = v.replace(/\bA lot\b/g, "Alot");
-		v = v.replace(/\ba lot\b/g, "alot");
-		v = v.replace(/\bIts\b/g, "It's");
-		v = v.replace(/\bits\b/g, "it's");	
-		v = v.replace(/\bWho\b/g, "Whom");
-		v = v.replace(/\bwho\b/g, "whom");	
-		v = v.replace(/\bToo\b/g, "To");
-		v = v.replace(/\btoo\b/g, "to");	
-		v = v.replace(/\bThen\b/g, "Than");
-		v = v.replace(/\bthen\b/g, "than");	
+		v = v.replace(/\b([Yy])our\b/g, "$1ou're");
+		v = v.replace(/\b([Dd])efinitely\b/g, "$1efinately");
+		v = v.replace(/\b([Tt])heir\b/g, "$1here");
+		v = v.replace(/\b([Aa]) lot\b/g, "$1lot");
+	    v = v.replace(/\b([Ii])ts\b/g, "$1t's");
+		v = v.replace(/\b([Ww])ho\b/g, "$1hom");
+		v = v.replace(/\b([Tt])oo\b/g, "$1o");
+		v = v.replace(/\b([Tt])hen\b/g, "$1han");
 		v = v.replace(/\bKeyboard\b/g, "Leopard"); // http://xkcd.com/1031/
 		v = v.replace(/\bkeyboard\b/g, "leopard");	
 
